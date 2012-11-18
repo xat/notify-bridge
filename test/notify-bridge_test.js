f0c7d1c7a9e4b14b91440b1daeec46f26870353a
Simon Kusterer <simon@soped.com>
2012-11-18 21:25:02 +0100

    implemented basic functionality

--- test/notify-bridge_test.js
+++ test/notify-bridge_test.js
@@ -1,34 +1,15 @@
 var notify_bridge = require('../lib/notify-bridge.js');
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+exports['client'] = {
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-
-exports['awesome'] = {
   setUp: function(done) {
-    // setup here
+    this.nb = notify_bridge.init();
     done();
   },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(notify_bridge.awesome(), 'awesome', 'should be awesome.');
+
+  'initialize': function(test) {
+    test.equal(typeof this.nb, 'object');
     test.done();
   }
+
 };
